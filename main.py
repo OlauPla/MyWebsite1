6a81cd7be4eef18a3bdfb561d191de7a01d64e1e
Olau Pla Richart <105815154+OlauPla@users.noreply.github.com>
2022-06-15 16:36:13 +0000

    Added blog

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+    
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
 
 @app.route('/contact')
 def contact():
@@ -21,4 +26,4 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host = '0.0.0.0')
